27201a4f4a59b180c8723623ef95064d8885adb6
Timothy Redmond <tredmond@stanford.edu>
2011-04-21 15:49:46 +0000

    moving to smi-tredmond-li

---
+++ src/main/java/org/protege/owl/codegeneration/SubstitutionVariable.java
@@ -0,0 +1,71 @@
+package org.protege.owl.codegeneration;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public enum SubstitutionVariable {
+	PACKAGE("package"),
+	JAVA_CLASS_NAME("javaClass"),
+	IMPLEMENTS_EXTENDS("extends"),
+	IRI("iri"),
+	CLASS("owlClass"), 
+	CAPITALIZED_CLASS("OwlClass"),
+	PROPERTY("owlProperty"), 
+	CAPITALIZED_PROPERTY("OwlProperty"),
+	PROPERTY_RANGE("propertyRange"),
+	DATE("date"),
+	USER("user");
+	
+	private static Map<String, String> templateMap = new HashMap<String, String>();
+	private String name;
+	
+	private SubstitutionVariable(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static String getTemplate(String resource) {
+		String template = templateMap.get(resource);
+		if (template == null) {
+			try {
+				URL u = SubstitutionVariable.class.getResource(resource);
+				Reader reader = new InputStreamReader(u.openStream());
+				StringBuffer buffer = new StringBuffer();
+				int charsRead;
+				char[] characters = new char[1024];
+				while (true) {
+					charsRead = reader.read(characters);
+					if (charsRead < 0) {
+						break;
+					}
+					buffer.append(characters, 0, charsRead - 1);
+				}
+				template = buffer.toString();
+				templateMap.put(resource, template);
+				reader.close();
+			}
+			catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return template;
+	}
+	
+	public static String fillTemplate(String resource, Map<SubstitutionVariable, String> substitutions) {
+		String result = getTemplate(resource);
+		for (Entry<SubstitutionVariable, String> entry : substitutions.entrySet()) {
+			SubstitutionVariable var = entry.getKey();
+			String replacement = entry.getValue();
+			result = result.replaceAll("\\$\\{" + var.getName() + "\\}", replacement);
+		}
+		return result;
+	}
+}
