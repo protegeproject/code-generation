9f46b7585a96665ae4b8364f8a7b2e660f327e6c
Timothy Redmond <tredmond@stanford.edu>
2011-04-26 14:00:33 +0000

    refactor - I did not understand the name of this class before

--- src/main/java/org/protege/editor/owl/codegeneration/GenerateCodeCallback.java
+++ src/main/java/org/protege/editor/owl/codegeneration/GenerateCodeCallback.java
@@ -3,11 +3,13 @@
  */
 package org.protege.editor.owl.codegeneration;
 
+import org.protege.editor.owl.model.OWLModelManager;
+
 /** 
  * @author z.khan
  *
  */
-public interface GenerateCodeWithOptions {
+public interface GenerateCodeCallback {
 
     /**
      * Called when user prompts to generate code
@@ -18,4 +20,6 @@ public interface GenerateCodeWithOptions {
      * Called when user cancels code generation
      */
     void cancelClicked();
+    
+    OWLModelManager getOWLModelManager();
 }
