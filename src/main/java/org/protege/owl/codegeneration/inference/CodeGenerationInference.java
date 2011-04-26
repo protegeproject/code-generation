74321b24d729eb4e9b842ad7ea2f925172917b79
Timothy Redmond <tredmond@stanford.edu>
2011-04-26 13:56:16 +0000

    struggling with java vs. owl incompatibility

--- src/main/java/org/protege/owl/codegeneration/inference/CodeGenerationInference.java
+++ src/main/java/org/protege/owl/codegeneration/inference/CodeGenerationInference.java
@@ -17,16 +17,44 @@ public interface CodeGenerationInference {
 	Collection<OWLNamedIndividual> getIndividuals(OWLClass owlClass);
 	
 	boolean canAs(OWLNamedIndividual i, OWLClass c);
-	
-	boolean canAssert(OWLNamedIndividual i, OWLClass c);
-	
+		
 	Collection<OWLClass> getTypes(OWLNamedIndividual i);
 	
+	/**
+	 * This method must satisfy two conditions:
+	 * <ul>
+	 * <li> For any named object property p and any named class C, if C&#8745;&#8707;p.Thing 
+	 *      is satisfiable then p must be a member of 
+	 *      getObjectPropertiesForClass(C).</li>
+	 * <li> For any named class C and any named class D, if it is known that C is a subclass of D then
+	 *      getObjectPropertiesForClass(C) must be a superset of getObjectPropertiesForClass(D).</li>
+	 * </ul>
+	 * <br/>
+	 * These are an artificial pair of constraints that result from the mismatch of java and OWL.
+	 * 
+	 * @param cls
+	 * @return
+	 */
 	Collection<OWLObjectProperty> getObjectPropertiesForClass(OWLClass cls);
 		
-	Collection<OWLClass> getRange(OWLClass cls, OWLObjectProperty p);
+	OWLClass getRange(OWLObjectProperty p);
 	
+	/**
+	 * This method must satisfy two conditions:
+	 * <ul>
+	 * <li> For any named data property p and any named class C, if C&#8745;&#8707;p.Literal 
+	 *      is satisfiable then p must be a member of 
+	 *      getDataPropertiesForClass(C).</li>
+	 * <li> For any named class C and any named class D, if it is known that C is a subclass of D then
+	 *      getDataPropertiesForClass(C) must be a superset of getDataPropertiesForClass(D).</li>
+	 * </ul>
+	 * <br/>
+	 * These are an artificial pair of constraints that result from the mismatch of java and OWL.
+	 * 
+	 * @param cls
+	 * @return
+	 */
 	Collection<OWLDataProperty> getDataPropertiesForClass(OWLClass cls);
 		
-	OWLDatatype getRange(OWLClass cls, OWLDataProperty p);
+	OWLDatatype getRange(OWLDataProperty p);
 }
