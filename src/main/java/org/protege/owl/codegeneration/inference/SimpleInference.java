26a02eadc894c6f409742926eeec855072535c67
Timothy Redmond <tredmond@stanford.edu>
2011-06-20 12:38:20 +0000

    The structural reasoner isSatisfiable was returning false.   I think that this is actually unexpected.

--- src/main/java/org/protege/owl/codegeneration/inference/SimpleInference.java
+++ src/main/java/org/protege/owl/codegeneration/inference/SimpleInference.java
@@ -1,19 +1,27 @@
 package org.protege.owl.codegeneration.inference;
 
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 
 public class SimpleInference extends ReasonerBasedInference {
-
+	private OWLOntology ontology;
 	
 	public SimpleInference(OWLOntology ontology) {
 		super(ontology, getSimpleReasoner(ontology));
+		this.ontology = ontology;
 	}
 	
 	private static OWLReasoner getSimpleReasoner(OWLOntology ontology) {
 		StructuralReasonerFactory factory = new StructuralReasonerFactory();
 		return factory.createReasoner(ontology);
+	}
+	
+	@Override
+	public boolean canAs(OWLNamedIndividual i, OWLClass c) {
+		return i.getTypes(ontology.getImportsClosure()).contains(c);
 	}
 	
 }
