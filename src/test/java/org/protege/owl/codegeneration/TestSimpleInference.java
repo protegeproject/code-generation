23b79638d7c9bde9ff5231733d6a8dbd3735b72d
Timothy Redmond <tredmond@stanford.edu>
2011-08-31 00:35:45 +0000

    the simple inference definitely needed some testing...

---
+++ src/test/java/org/protege/owl/codegeneration/TestSimpleInference.java
@@ -0,0 +1,53 @@
+package org.protege.owl.codegeneration;
+
+import org.protege.owl.codegeneration.inference.CodeGenerationInference;
+import org.protege.owl.codegeneration.inferred.pizza.CheeseyPizza;
+import org.protege.owl.codegeneration.inferred.pizza.MyPizzaFactory;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSimpleInference {
+
+    @Test
+    public void testInferredSubclasses() throws Exception {
+        MyPizzaFactory factory = TestUtilities.openFactory(TestUtilities.PIZZA_ONTOLOGY, MyPizzaFactory.class, false);
+        CodeGenerationInference inference = factory.getInference();
+        OWLClass pizza = org.protege.owl.codegeneration.inferred.pizza.Vocabulary.PIZZA;
+        OWLClass cheeseyPizza = org.protege.owl.codegeneration.inferred.pizza.Vocabulary.CHEESEYPIZZA;
+        
+        Assert.assertTrue(inference.getSubClasses(pizza).contains(cheeseyPizza));
+        OWLOntology ontology = factory.getOwlOntology();
+        OWLDataFactory owlapiFactory = ontology.getOWLOntologyManager().getOWLDataFactory();
+        OWLAxiom subClassAxiom = owlapiFactory.getOWLSubClassOfAxiom(pizza, cheeseyPizza);
+        Assert.assertFalse(factory.getOwlOntology().containsAxiom(subClassAxiom));
+    }
+    
+    @Test
+    public void testInferredSuperclasses() throws Exception {
+        MyPizzaFactory factory = TestUtilities.openFactory(TestUtilities.PIZZA_ONTOLOGY, MyPizzaFactory.class, false);
+        CodeGenerationInference inference = factory.getInference();
+        OWLClass pizza = org.protege.owl.codegeneration.inferred.pizza.Vocabulary.PIZZA;
+        OWLClass cheeseyPizza = org.protege.owl.codegeneration.inferred.pizza.Vocabulary.CHEESEYPIZZA;
+        
+        Assert.assertTrue(inference.getSuperClasses(cheeseyPizza).contains(pizza));
+        OWLOntology ontology = factory.getOwlOntology();
+        OWLDataFactory owlapiFactory = ontology.getOWLOntologyManager().getOWLDataFactory();
+        OWLAxiom subClassAxiom = owlapiFactory.getOWLSubClassOfAxiom(pizza, cheeseyPizza);
+        Assert.assertFalse(factory.getOwlOntology().containsAxiom(subClassAxiom));
+    }
+    
+    @Test
+    public void testIndirectTypes() throws Exception {
+        MyPizzaFactory factory = TestUtilities.openFactory(TestUtilities.PIZZA_ONTOLOGY, MyPizzaFactory.class, false);
+        CodeGenerationInference inference = factory.getInference();
+        CheeseyPizza cp = factory.createCheeseyPizza(TestUtilities.PIZZA_NS + "#myVeryCheeseyPizza");
+        OWLClass domain = org.protege.owl.codegeneration.inferred.pizza.Vocabulary.DOMAINCONCEPT;
+        Assert.assertTrue(inference.canAs(cp.getOwlIndividual(), domain));
+    }
+    
+    
+}
